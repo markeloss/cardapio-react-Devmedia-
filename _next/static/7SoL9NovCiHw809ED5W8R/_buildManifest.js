4dcf272e65c81bf3bf9f3f206e63ed9e710e27d4
markeloss <61366277+markeloss@users.noreply.github.com>
2024-09-01 00:24:21 +0000

    Deploying to gh-pages from @ markeloss/cardapio-react-Devmedia-@ae4eea5992c94ce9f31fd7941297fc667af19787 🚀

---
+++ _next/static/7SoL9NovCiHw809ED5W8R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/6a7ff8d13f2ab990.css","static/chunks/pages/index-b8d5279c73c11162.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
