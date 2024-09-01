dc1466f5ad3d685351308b8a9a575dbb07b367e7
markeloss <eriquepinon@gmail.com>
2024-08-31 21:15:34 -0300

    github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/</barbearia-simples-Devmedia->',
+  assetPrefix: '/</barbearia-simples-Devmedia->/',
 };
 
 export default nextConfig;
