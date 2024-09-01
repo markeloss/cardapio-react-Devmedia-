ae4eea5992c94ce9f31fd7941297fc667af19787
markeloss <eriquepinon@gmail.com>
2024-08-31 21:22:44 -0300

    imagens

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,10 @@ const nextConfig = {
   output: 'export',
   basePath: '/</barbearia-simples-Devmedia->',
   assetPrefix: '/</barbearia-simples-Devmedia->/',
-};
-
-module.exports = {
   images: {
     unoptimized: true,
   },
 };
+
+
 export default nextConfig;
